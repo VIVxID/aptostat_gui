d827106689f275a3601f155fc27bd71d46ed60d6
Maud <maud.lundh@gmail.com>
2013-04-04 12:05:31 +0200

    temp vardump

--- list_incidents.php
+++ list_incidents.php
@@ -35,6 +35,8 @@
             $result = json_decode($result_json, true);
             $incidents = $result["report"]["incidents"];
             ksort($incidents);
+            
+            var_dump($incidents);
         ?>
         
         <div class="container_12 content_box">
@@ -69,75 +71,7 @@
                     Details
                 </div>
                 <div class="groupbox_wrapper">
-                    <div class="front_messagebox">
-                        <table border="0">
-                             <tr>
-                                <td>ID:</td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>Startdate:</td>
-                                <td>2013-03-12</td>
-                            </tr>
-                            <tr>
-                                <td>Included reports:</td>
-                                <td>38, 39, 40</td>
-                            </tr>
-                            <tr>
-                                <td>Services:</td>
-                                <td>DrVideo API</td>
-                            </tr>
-                            <tr>
-                                <td>Status:</td>
-                                <td>Critical</td>
-                            </tr>
-                        </table>
-                        <hr />
-                        Message from Support:<br />
-                        This is a test message to test this box!<br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam molestie ullamcorper rutrum. Nunc sit amet nunc a quam viverra blandit eleifend eget urna. Duis adipiscing fringilla rhoncus. Suspendisse vitae purus sed nisl euismod consectetur eu ut neque. Nunc vel nulla eget lectus volutpat adipiscing. Quisque mollis magna lectus. Maecenas quis risus metus. Vestibulum fermentum ligula varius elit suscipit quis accumsan turpis mollis. In hendrerit aliquet magna nec ultricies. Nullam eget nunc ac sem accumsan imperdiet. Nam tellus lacus, imperdiet lacinia scelerisque id, mollis id enim. Mauris lobortis erat ut ipsum molestie suscipit vel eget magna. 
-                        <hr />
-                        <div class="author">
-                            Author: Shakespeare
-                        </div>
-                        <div class="edit">
-                            <a href="edit_incident.php">Edit incident</a>
-                        </div>
-                        <br/>
-                    </div>
-                   
-                    <div class="front_messagebox">
-                        <table border="0">
-                            <tr>
-                                <td>Id Incident:</td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>Startdate:</td>
-                                <td>2013-03-12</td>
-                            </tr>
-                            <tr>
-                                <td>Services:</td>
-                                <td>DrVideo API</td>
-                            </tr>
-                            <tr>
-                                <td>Status:</td>
-                                <td>Resolved</td>
-                            </tr>
-                        </table>
-                        <hr />
-                        Message from Support:<br />
-                        This is yet another test message to test this box!<br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam molestie ullamcorper rutrum. Nunc sit amet nunc a quam viverra blandit    eleifend eget urna. Duis adipiscing fringilla rhoncus. Suspendisse vitae purus sed nisl euismod consectetur eu ut neque. Nunc vel nulla eget lectus volutpat adipiscing. Quisque mollis magna lectus. Maecenas quis risus metus. Vestibulum fermentum ligula varius elit suscipit quis accumsan turpis mollis. In hendrerit aliquet magna nec ultricies. Nullam eget nunc ac sem accumsan imperdiet. Nam tellus lacus, imperdiet lacinia scelerisque id, mollis id enim. Mauris lobortis erat ut ipsum molestie suscipit vel eget magna.
-                            <hr />
-                            <div class="author">
-                                Author: Shakespeare
-                            </div>
-                            <div class="edit">
-                                <a href="edit_incident.php">Edit incident</a>
-                            </div>
-                            <br/>
-                    </div>
+                    
                 </div>
                 <div class="list_content_menu_fat">
                     <a href="http://apto.vlab.iu.hio.no/edit_incident.php" style="float: right; margin-right: 5px; margin-top: 10px;">Make new message</a>
