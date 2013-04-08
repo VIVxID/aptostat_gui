80a88001b6eb77f615985b72860866b8206aeb9d
Even <even248@gmail.com>
2013-04-08 13:40:40 +0200

    view incident fix

--- ajax/viewIncident.php
+++ ajax/viewIncident.php
@@ -22,16 +22,15 @@
     $result_json = curl_exec($ch);
     $result = json_decode($result_json, true);
     $incident = $result["incident"];
-    
-    foreach($incident as $reports => $report) {
-        $idreport = $reports["idReport"];
-        }
-   
+    $reports = $incident["connectedReports"];
     
     print "Incident ID: " . $incident["idIncident"] . "<br />";
     print "Timestamp: " . $incident["timestamp"] . "<br />";
     print "Last Flag: " . $incident["lastFlag"] . "<br />";
-    print "Connected Reports: " . $idreport . "<br /><br />";
+    print "Connected Reports: " . foreach($reports as $report) {
+        $idReport = $report["idReport"];
+        print $idReport . ", ";
+    } . "<br /><br />";
     print "Last Message Date: " . $incident["lastMessageDate"] . "<br />";
     print "Last Message: " . $incident["lastMessage"] . "<br />";
     print "Connected Messages " . $incident["connectedMessages"] . "<br />";
