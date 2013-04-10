40d1558a2378de7f973e3d0ec24027db662ec87c
Even Geithus Owre <even248@gmail.com>
2013-04-10 23:47:02 +0200

    New message button anchor URL removed

--- list_incidents.php
+++ list_incidents.php
@@ -73,7 +73,7 @@
                         Click on an incident to view it and to make or edit messages for the incident.
                 </div>
                 <div class="list_content_menu_fat">
-                    <a href="http://apto.vlab.iu.hio.no/edit_incident.php" id="newMessage" style="float: right; margin-right: 5px; margin-top: 10px; display: none;">Make new message</a>
+                    <a href="#" id="newMessage" style="float: right; margin-right: 5px; margin-top: 10px; display: none;">Make new message</a>
                 </div>
             </div>
         </div>
