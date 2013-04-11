166442a927b8497cd130598d797d5d1640a90dbe
Maud <maud.lundh@gmail.com>
2013-04-12 01:08:22 +0200

    added json

--- make_incident.php
+++ make_incident.php
@@ -13,4 +13,47 @@
                 <a href="http://apto.vlab.iu.hio.no/admin_front.php"><img id="logo" src="img/logo.png" /></a>
             </div>
         </header>
-        k
+        
+        <?php
+                //API URL Report
+                $json_url = APIURL . "report";
+                
+                //initializing curl
+                $ch = curl_init($json_url);
+                
+                //Curl options
+                $options = array(
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => "GET"
+                );
+                
+                //setting curl options
+                curl_setopt_array($ch, $options);
+                
+                //getting results
+                $result_json = curl_exec($ch);
+                $result = json_decode($result_json, true);
+                $groups = $result["report"]["groups"];
+                ksort($groups);
+                
+                //API URL Incident
+                 $json_url = APIURL . "incident";
+                
+                //initializing curl
+                $ch = curl_init($json_url);
+                
+                //Curl options
+                $options = array(
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => "GET"
+                );
+                
+                //setting curl options
+                curl_setopt_array($ch, $options);
+                
+                //getting results
+                $result_json = curl_exec($ch);
+                $result = json_decode($result_json, true);
+                $incidents = $result["incident"]["incidents"];
+                ksort($incidents);
+            ?>
