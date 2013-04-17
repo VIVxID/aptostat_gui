0e354b22d34d4e9b4289726dacac0a0408eeb33e
Ketil Ovrebo <ketilovre@gmail.com>
2013-04-17 21:32:52 +0200

    Added symbols to the group list

--- admin/inc/reports.php
+++ admin/inc/reports.php
@@ -35,24 +35,45 @@ class Reports
 
         foreach($groups as $group => $reports) { //gets service and its reports
 
-            if (count($reports) > 1){
-                $countString = count($reports)." errors";
-            } else {
-                $countString = "1 error";
+            foreach ($reports as $report) {
+                if ($report["flag"] == "WARNING") {
+                    $warningsExist = 1;
+                } elseif ($report["flag"] == "CRITICAL") {
+                    $criticalsExist = 1;
+                }
             }
+
+            if (isset($criticalsExist)) {
+                $titleImage = "../img/cross.png";
+            } elseif (isset($warningsExist)) {
+                $titleImage = "../img/warning.png";
+            } else {
+                $titleImage = "../img/check.png";
+            }
+
+            if (!isset($warningsExist) and !isset($criticalsExist)) {
+                $titleErrorString = "All responding";
+            } elseif (count($reports) > 1){
+                $titleErrorString = count($reports)." errors";
+            } else {
+                $titleErrorString = "1 error";
+            }
+
+            unset($criticalsExist);
+            unset($warningsExist);
 
             print "<div class='accordion-group'>\r\n";
             print "<div class='accordion-heading'>";
-            print "<a class='accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion2' href='#".$collapseOrder."'>".$group." - $countString</a>\r\n";
+            print "<a class='accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion2' href='#".$collapseOrder."'><img class='right' src='$titleImage' />".$group." - $titleErrorString</a>\r\n";
             print "</div>\r\n";
             print "<div id='".$collapseOrder."' class='accordion-body collapse'>\r\n";
             print "<div class='accordion-inner'>\r\n";
             print "<ol>\r\n";
             foreach($reports as $report) { //goes through all reports for the service
-                print "<li class='report file ui-widget-content' id='report_".$report["id"]."'>";//the .file class makes it clickable for ajax loading of the report
-                print "Error #".$report["id"]." - ".$report["flag"]."\r\n";
-                print "<p class='tinytext'>Check type: ".$report["checkType"]."</p>";
-                print "<p class='tinytext'>Error message: ".$report["errorMessage"]."</p>";
+                print "<li class='report file ui-widget-content' id='report_".$report["id"]."'>\r\n";//the .file class makes it clickable for ajax loading of the report
+                print "Error #".$report["id"]." - ".ucwords(strtolower($report["flag"]))."\r\n";
+                print "<p class='tinytext'>Check type: ".$report["checkType"]."</p>\r\n";
+                print "<p class='tinytext'>Error message: ".$report["errorMessage"]."</p>\r\n";
             }
             print "</li>\r\n";
             print "</ol>\r\n";
