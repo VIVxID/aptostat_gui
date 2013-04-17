8565d3f2fa63c759ed01dce3fdfc9db26b96f913
Even Geithus Owre <even248@gmail.com>
2013-04-17 16:36:25 +0200

    Combined group_reports and list_incidents. Hope this won't break everything.

--- admin/group_reports.php
+++ admin/group_reports.php
@@ -40,65 +40,127 @@
                 $groups[$report["host"]][] = $report;
 
             }
-
-
             ksort($groups);
+
+            //API URL
+            $json_url = APIURL . "incident";
+
+            //initializing curl
+            $ch = curl_init($json_url);
+
+            //Curl options
+            $options = array(
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => "GET"
+            );
+
+            //setting curl options
+            curl_setopt_array($ch, $options);
+
+            //getting results
+            $result_json = curl_exec($ch);
+            $result = json_decode($result_json, true);
+            $incidents = $result["incidents"];
+            ksort($incidents);
+
         ?>
-        
-        <div class="container">
-            <div class="row">
-                <div class="list_content" id="groupbox_reports">
-                    <div class="groupbox_heading">
-                        Open reports
-                    </div>
-                    <div class="groupbox_wrapper">
-                        <div class="accordion" id="accordion2">
-                            <?php
-                                foreach($groups as $group => $reports) { //gets service and its reports
 
-                                    if (count($reports) > 1){
-                                        $countString = count($reports)." errors";
-                                    } else {
-                                        $countString = "1 error";
-                                    }
+        <div id="content_wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="tabbable">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab1" data-toggle="tab">Reports</a></li>
+                            <li><a href="#tab2" data-toggle="tab">Incidents</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab1">
+                                <div class="list_content" id="groupbox_reports">
+                                    <div class="groupbox_heading">
+                                        Open reports
+                                    </div>
+                                    <div class="groupbox_wrapper">
+                                        <div class="accordion" id="accordion2">
+                                            <?php
+                                            foreach($groups as $group => $reports) { //gets service and its reports
 
-                                    print "<div class='accordion-group'>\r\n";
-                                        print "<div class='accordion-heading'>";
-                                        print "<a class='accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion2' href='#".$collapseOrder."'>".$group." - $countString</a>\r\n";
-                                        print "</div>\r\n";
-                                        print "<div id='".$collapseOrder."' class='accordion-body collapse'>\r\n";
-                                        print "<div class='accordion-inner'>\r\n";
-                                        print "<ol>\r\n";
-                                        foreach($reports as $report) { //goes through all reports for the service
-                                            print "<li class='report ui-widget-content' id='report_".$report["id"]."'>";//the .file class makes it clickable for ajax loading of the report
-                                            print "Error #".$report["id"]." - ".$report["flag"]."\r\n";
-                                            print "<p class='tinytext'>Check type: ".$report["checkType"]."</p>";
-                                            print "<p class='tinytext'>Error message: ".$report["errorMessage"]."</p>";
-                                        }
-                                        print "</li>\r\n";
-                                        print "</ol>\r\n";
-                                        print "</div>\r\n";
-                                        print "</div>\r\n";
-                                    print "</div>\r\n";
-                                    $collapseOrder++;
-                                }
-                            ?>
+                                                if (count($reports) > 1){
+                                                    $countString = count($reports)." errors";
+                                                } else {
+                                                    $countString = "1 error";
+                                                }
+
+                                                print "<div class='accordion-group'>\r\n";
+                                                print "<div class='accordion-heading'>";
+                                                print "<a class='accordion-toggle collapsed' data-toggle='collapse' data-parent='#accordion2' href='#".$collapseOrder."'>".$group." - $countString</a>\r\n";
+                                                print "</div>\r\n";
+                                                print "<div id='".$collapseOrder."' class='accordion-body collapse'>\r\n";
+                                                print "<div class='accordion-inner'>\r\n";
+                                                print "<ol>\r\n";
+                                                foreach($reports as $report) { //goes through all reports for the service
+                                                    print "<li class='report ui-widget-content' id='report_".$report["id"]."'>";//the .file class makes it clickable for ajax loading of the report
+                                                    print "Error #".$report["id"]." - ".$report["flag"]."\r\n";
+                                                    print "<p class='tinytext'>Check type: ".$report["checkType"]."</p>";
+                                                    print "<p class='tinytext'>Error message: ".$report["errorMessage"]."</p>";
+                                                }
+                                                print "</li>\r\n";
+                                                print "</ol>\r\n";
+                                                print "</div>\r\n";
+                                                print "</div>\r\n";
+                                                print "</div>\r\n";
+                                                $collapseOrder++;
+                                            }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="list_content_menu">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="tab2">
+                                <div class="list_content" id="incidentbox_list">
+                                    <div class="groupbox_heading">
+                                        Incidents
+                                    </div>
+                                    <div class="groupbox_wrapper">
+                                        <ul>
+                                            <?php
+                                            foreach($incidents as $incident) {
+                                                $date = $incident["createdTimestamp"];
+                                                $title = $incident["title"];
+                                                print "<li class='file' id='incident_".$incident["id"]."'>Incident ".$incident["id"]." - $date - $title</li>\r\n";
+
+                                            }
+                                            ?>
+                                        </ul>
+                                    </div>
+                                    <div class="list_content_menu_fat">
+                                        View all <input type="checkbox"/>
+                                        Warning <input type="checkbox"/>
+                                        Ignored <input type="checkbox"/>
+                                        <br/>
+                                        Critical <input type="checkbox"/>
+                                        Resolved <input type="checkbox"/>
+                                        Internal <input type="checkbox"/>
+                                        Responding <input type="checkbox"/>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="list_content_menu">
-                        
-                    </div>
-                </div>
-                
-                <div class="list_content" id="groupbox_details">
-                    <div class="groupbox_heading">
-                        Details for selected report
-                    </div>
-                    <div class="groupbox_wrapper" id="reportPane">
-                        Click a report to view it.
-                    </div>
-                    <div class="list_content_menu">
-                        
+
+
+                    <div class="list_content" id="groupbox_details">
+                        <div class="groupbox_heading">
+                            Details for selected report
+                        </div>
+                        <div class="groupbox_wrapper" id="reportPane">
+                            Click a report to view it.
+                        </div>
+                        <div class="list_content_menu">
+
+                        </div>
                     </div>
                 </div>
             </div>
