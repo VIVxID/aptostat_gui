ff871dcccf95765b8e6347fc0e651e935b0e8d6f
Maud <maud.lundh@gmail.com>
2013-04-18 15:57:25 +0200

    taken away dropdown, goes for a normal navbar

--- inc/html_head.php
+++ inc/html_head.php
@@ -28,22 +28,15 @@
     <header id="header">
         <div id="header-content">
 
-            <a href="admin_front.php"><img id="logo" src="/img/logo.png" /></a>
+           <a href="index.php"><img id="logo" src="/img/logo.png" /></a>
             
-             <!-- Drop down menu-->
-    <div class="dropdown">
-          <a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
-            Dropdown
-            <b class="caret"></b>
-          </a>
-          <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+           <div class="menubar">
             <a href="group_reports.php">Panel</a>
             <a href="group_reports.php">Manage</a>
             <a href="group_reports.php">Twitter</a>
-          </ul>
-</div>
+     </div>
 
-            <a href="index.php"><img id="logo" src="/img/logo.png" /></a>
+            
 
         </div>
     </header>
