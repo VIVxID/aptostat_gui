8ceb2687a92784d447f71d8825f812166d1a1e74
Maud <maud.lundh@gmail.com>
2013-04-18 10:41:23 +0200

    font-size and weight defined in tree.css. Minor size changes in apto_stylesheet under current box and message box. Deleted <b>tags in live table

--- inc/live_table.php
+++ inc/live_table.php
@@ -9,52 +9,75 @@ $options = array(
 );
 
 //setting curl options
+
 curl_setopt_array($curl, $options);
 
 //getting results
+
 $result_json = curl_exec($curl);
 $result = json_decode($result_json, true);
 ksort($result);
+
 foreach($result as $service => $state) {
+
     $product = explode(" ", $service); //get product name
+    
     $tree[$product[0]][$product[1]] = $state; //put each service and its state under the corresponding product entry
 }
 
 print "<ol class='tree'>\r\n";
 
 foreach($tree as $product => $array) {
+
     foreach($array as $service => $state) { //check status of services
+    
         switch($state) {
+        
             case "up":
                 $$product = "up";
+                
                 break;
+                
             case "down":
                 $$product = "down";
+                
                 break;
+                
             default:
                 $$product = "warning";
+                
                 break;
         }
     }
 
     print "<li>\r\n";
+    
     if($$product == "down") {
-        print "<label for='$product'><b>$product <img src='../img/cross.png' /> Down</b></label>\r\n".
+    
+        print 
+            "<label for='$product'>$product <img src='../img/cross.png' /> Down</label>\r\n".
             "<input type='checkbox' checked id='$product' />\r\n";
     }
     elseif($$product == "warning") {
-        print "<label for='$product'><b>$product <img src='../img/warning.png' /> Problem</b></label>\r\n".
+    
+        print 
+            "<label for='$product'>$product <img src='../img/warning.png' /> Problem</label>\r\n".
             "<input type='checkbox' checked id='$product' />\r\n";
     }
     else {
-        print "<label for='$product'><b>$product <img src='../img/check.png' /> OK</b></label>\r\n".
+    
+        print 
+            "<label for='$product'>$product <img src='../img/check.png' /> OK</label>\r\n".
             "<input type='checkbox' id='$product' />\r\n";
     }
 
     print "<ol>\r\n";
+    
     foreach($array as $service => $state) {
+    
         print "<li class='file'>\r\n";
         print "$service ";
+        
         //validating state
         if ($state == "up") {
             print "<img src='../img/check.png' /> OK\r\n";
