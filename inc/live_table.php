5e5406cacbb93450a93e0de6721d13e97049f747
Even <even248@gmail.com>
2013-04-18 14:49:37 +0200

    Moved service labels to the right.

--- inc/live_table.php
+++ inc/live_table.php
@@ -24,73 +24,74 @@ foreach($result as $service => $state) {
     
     $tree[$product[0]][$product[1]] = $state; //put each service and its state under the corresponding product entry
 }
+print "<div id='service-panel-wrapper'>";
+    print "<ol class='tree'>\r\n";
 
-print "<ol class='tree'>\r\n";
+    foreach($tree as $product => $array) {
 
-foreach($tree as $product => $array) {
+        foreach($array as $service => $state) { //check status of services
 
-    foreach($array as $service => $state) { //check status of services
-    
-        switch($state) {
-        
-            case "up":
-                $$product = "up";
-                
-                break;
-                
-            case "down":
-                $$product = "down";
-                
-                break;
-                
-            default:
-                $$product = "warning";
-                
-                break;
+            switch($state) {
+
+                case "up":
+                    $$product = "up";
+
+                    break;
+
+                case "down":
+                    $$product = "down";
+
+                    break;
+
+                default:
+                    $$product = "warning";
+
+                    break;
+            }
         }
-    }
 
-    print "<li>\r\n";
-    
-    if($$product == "down") {
-    
-        print 
-            "<label for='$product'>$product <span class='service-status'><img src='../img/cross.png' /> Down</span></label>\r\n".
-            "<input type='checkbox' checked id='$product' />\r\n";
-    }
-    elseif($$product == "warning") {
-    
-        print 
-            "<label for='$product'>$product <span class='service-status'><img src='../img/warning.png' /> Problem</span></label>\r\n".
-            "<input type='checkbox' checked id='$product' />\r\n";
-    }
-    else {
-    
-        print 
-            "<label for='$product'>$product <span class='service-status'><img src='../img/check.png' /> OK</span></label>\r\n".
-            "<input type='checkbox' id='$product' />\r\n";
-    }
+        print "<li>\r\n";
 
-    print "<ol>\r\n";
-    
-    foreach($array as $service => $state) {
-    
-        print "<li class='file'>\r\n";
-        print "$service ";
-        
-        //validating state
-        if ($state == "up") {
-            print "<img src='../img/check.png' /> OK\r\n";
+        if($$product == "down") {
+
+            print
+                "<label for='$product'>$product <span class='service-status'>Down <img src='../img/cross.png' /></span></label>\r\n".
+                "<input type='checkbox' checked id='$product' />\r\n";
         }
-        elseif ($state == "down") {
-            print "<img src='../img/cross.png' /> Down\r\n";
+        elseif($$product == "warning") {
+
+            print
+                "<label for='$product'>$product <span class='service-status'>Problem <img src='../img/warning.png' /></span></label>\r\n".
+                "<input type='checkbox' checked id='$product' />\r\n";
         }
         else {
-            print "<img src='../img/warning.png' /> Problem\r\n";
+
+            print
+                "<label for='$product'>$product <span class='service-status'>OK <img src='../img/check.png' /></span></label>\r\n".
+                "<input type='checkbox' id='$product' />\r\n";
         }
-        print "<br />\r\n";
+
+        print "<ol>\r\n";
+
+        foreach($array as $service => $state) {
+
+            print "<li class='file'>\r\n";
+            print "$service ";
+
+            //validating state
+            if ($state == "up") {
+                print "<img src='../img/check.png' /> OK\r\n";
+            }
+            elseif ($state == "down") {
+                print "<img src='../img/cross.png' /> Down\r\n";
+            }
+            else {
+                print "<img src='../img/warning.png' /> Problem\r\n";
+            }
+            print "<br />\r\n";
+        }
+        print "</ol>\r\n";
+        print "</li>\r\n";
     }
     print "</ol>\r\n";
-    print "</li>\r\n";
-}
-print "</ol>\r\n";
+print "</div>";
