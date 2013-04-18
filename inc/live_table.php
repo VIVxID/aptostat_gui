2f5bff5a12172753d7ef91c916191adba44d11e1
Even <even248@gmail.com>
2013-04-18 14:58:03 +0200

    Beautifying live panel.

--- inc/live_table.php
+++ inc/live_table.php
@@ -55,18 +55,21 @@ print "<div id='service-panel-wrapper'>";
         if($$product == "down") {
 
             print
+                "<hr>".
                 "<label for='$product'>$product <span class='service-status'>Down <img src='../img/cross.png' /></span></label>\r\n".
                 "<input type='checkbox' checked id='$product' />\r\n";
         }
         elseif($$product == "warning") {
 
             print
+                "<hr>".
                 "<label for='$product'>$product <span class='service-status'>Problem <img src='../img/warning.png' /></span></label>\r\n".
                 "<input type='checkbox' checked id='$product' />\r\n";
         }
         else {
 
             print
+                "<hr>".
                 "<label for='$product'>$product <span class='service-status'>OK <img src='../img/check.png' /></span></label>\r\n".
                 "<input type='checkbox' id='$product' />\r\n";
         }
