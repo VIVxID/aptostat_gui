9f36bb084a44eb03592aff123fff062291334ef9
Ketil <ketilovre@gmail.com>
2013-04-18 10:57:28 +0200

    Set up the admin front page

--- app/routing.php
+++ app/routing.php
@@ -19,3 +19,23 @@ $app->get('/', function() use ($app) {
 
     return $app['twig']->render('index.twig', $includeBag);
 });
+
+// Index: CustomerFrontEnd
+$app->get('/admin', function() use ($app) {
+    $liveService = new \aptostatGui\Service\LiveService();
+    $realTime = $liveService->getLiveAsArray();
+
+    $uptimeService = new \aptostatGui\Service\UptimeService();
+    $uptime = $uptimeService->getUptimeAsArray();
+
+    $messageService = new \aptostatGui\Service\MessageService();
+    $messageHistory = $messageService->getMessageHistoryAsArray();
+
+    $includeBag = array(
+        'realTime' => $realTime,
+        'uptime' => $uptime,
+        'messageHistory' => $messageHistory,
+    );
+
+    return $app['twig']->render('index.twig', $includeBag);
+});
