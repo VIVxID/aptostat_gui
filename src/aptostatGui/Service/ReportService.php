9878c1b54761ec0a738a1435fe66f71580568911
Ketil <ketilovre@gmail.com>
2013-04-19 19:11:04 +0200

    Created ReportService

---
+++ src/aptostatGui/Service/ReportService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace aptostatGui\Service;
+
+
+class Report
+{
+
+    public function getReportsAsArray
+    {
+
+        $apiService = new ApiService();
+        $reports = $apiService->getReportList();
+
+        foreach ($reports["reports"] as $report) {
+
+            if ($report["flag"] != "RESOLVED") {
+                $groups[$report["host"]][] = $report;
+            }
+
+        }
+        ksort($groups);
+
+        return $groups;
+
+    }
+
+}
