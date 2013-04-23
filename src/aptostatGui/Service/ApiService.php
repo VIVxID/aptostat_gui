cb8e9174a13cdddb3870288cd92f020b3f5512d0
Noha Xue <noxu27@gmail.com>
2013-04-23 09:55:42 +0200

    Fixed admin front page

--- src/aptostatGui/Service/ApiService.php
+++ src/aptostatGui/Service/ApiService.php
@@ -35,13 +35,72 @@ class ApiService
         return $this->getDataFromApi('api/live');
     }
 
+    //TODO:
+    public function postIncident($)
+    {
+        return $this->postDataToApi('api/incident', $postDataAsJson);
+    }
+
+    public function postMessage($incidentId, $post)
+
     private function getDataFromApi($subUrl)
     {
         $curl = curl_init();
         $options = array(
             CURLOPT_URL => APIURL . $subUrl,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => "GET"
+            CURLOPT_CUSTOMREQUEST => 'GET'
+        );
+
+        curl_setopt_array($curl, $options);
+
+        $result = json_decode(curl_exec($curl), true);
+
+        if (is_null($result)) {
+            throw new \Exception('Failed to connect to API server', 500);
+        }
+
+        if (isset($result['error'])) {
+            throw new \Exception($result['error']['errorMessage'], $result['error']['statusCode']);
+        }
+        return $result;
+    }
+
+    private function postDataToApi($subUrl, $postDataAsJson)
+    {
+        $curl = curl_init();
+        $options = array(
+            CURLOPT_URL => APIURL . $subUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postDataAsJson,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($postDataAsJson),
+            ),
+        );
+
+        curl_setopt_array($curl, $options);
+
+        $response = json_decode(curl_exec($curl), true);
+
+        if (is_null($response)) {
+            throw new \Exception('Failed to connect to API server', 500);
+        }
+
+        if (isset($response['error'])) {
+            throw new \Exception($response['error']['errorMessage'], $response['error']['statusCode']);
+        }
+        return $response;
+    }
+
+    private function putDataToApi($subUrl)
+    {
+        $curl = curl_init();
+        $options = array(
+            CURLOPT_URL => APIURL . $subUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'GET'
         );
 
         curl_setopt_array($curl, $options);
--- src/aptostatGui/controller/adminIndexController.php
+++ src/aptostatGui/controller/adminIndexController.php
@@ -1,19 +1,44 @@
 <?php
 
 $app->get('/admin', function() use ($app) {
+
+    // Real-time status module
+    try {
+        $liveService = new aptostatGui\Service\LiveService();
+        $realTime = $liveService->getLiveAsArray();
+    } catch (Exception $e) {
+        $realTime = null;
+        $app['monolog']->addCritical('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
+    }
+
+    // Last 7 days uptime table
+    try {
+        $uptimeService = new \aptostatGui\Service\UptimeService();
+        $uptime = $uptimeService->getUptimeAsArray();
+    } catch (Exception $e) {
+        $uptime = null;
+        $app['monolog']->addCritical('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
+    }
+
+    // Last messages past 3 days module
+    try {
+        $messageService = new \aptostatGui\Service\MessageService();
+        $messageHistory = $messageService->getMessageHistoryAsArray(3);
+    } catch (Exception $e) {
+        $messageHistory = null;
+        $app['monolog']->addCritical('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
+    }
+
+    // Current incidents module
+    try {
+        $incidentService = new aptostatGui\Service\IncidentService();
+        $currentIncidents = $incidentService->getCurrentIncidentsAsArray();
+    } catch (Exception $e) {
+        $currentIncidents = null;
+        $app['monolog']->addCritical('Error: ' . $e->getMessage() . ' Code: ' . $e->getCode());
+    }
+
     $token = $app['security']->getToken();
-
-    $liveService = new \aptostatGui\Service\LiveService();
-    $realTime = $liveService->getLiveAsArray();
-
-    $uptimeService = new \aptostatGui\Service\UptimeService();
-    $uptime = $uptimeService->getUptimeAsArray();
-
-    $messageService = new \aptostatGui\Service\MessageService();
-    $messageHistory = $messageService->getMessageHistoryAsArray(3);
-
-    $incidentService = new aptostatGui\Service\IncidentService();
-    $currentIncidents = $incidentService->getCurrentIncidentsAsArray();
 
     $includeBag = array(
         'realTime' => $realTime,
