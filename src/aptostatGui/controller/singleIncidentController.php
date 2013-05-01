4a67cf97ce46b427ca74fa57579a7aad96cd8838
Noha Xue <noxu27@gmail.com>
2013-05-01 19:08:07 +0200

    Removed sensitive info + fixed sorting of messages

--- src/aptostatGui/controller/singleIncidentController.php
+++ src/aptostatGui/controller/singleIncidentController.php
@@ -4,11 +4,9 @@ $app->get('/incident/{id}', function($id) use ($app) {
     try {
         $apiService = new aptostatGui\Service\ApiService();
         $incident = $apiService->getIncidentById($id);
-        $connectedReports = $apiService->getConnectedReports($id);
 
         $includeBag = array(
             'incident' => $incident['incidents'],
-            'connectedReports' => $connectedReports['reports'],
         );
 
         return $app['twig']->render('singleIncident.twig', $includeBag);
