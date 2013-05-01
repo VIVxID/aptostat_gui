4cdbf5ce797b81e112b7d270047912b0d0ca2d69
Noha Xue <noxu27@gmail.com>
2013-05-01 18:47:48 +0200

    Coded a view for a single incident

--- app/routing.php
+++ app/routing.php
@@ -4,4 +4,4 @@ include __DIR__ . '/../src/aptostatGui/controller/customerIndexController.php';
 include __DIR__ . '/../src/aptostatGui/controller/adminIndexController.php';
 include __DIR__ . '/../src/aptostatGui/controller/manageController.php';
 include __DIR__ . '/../src/aptostatGui/controller/ajaxController.php';
-
+include __DIR__ . '/../src/aptostatGui/controller/singleIncidentController.php';
