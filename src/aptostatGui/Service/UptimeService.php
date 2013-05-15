5081c527ed79724a43cbf8c703ce0eb60db2ece8
Ketil <ketilovre@gmail.com>
2013-05-15 12:29:11 +0200

    Fixed a latent error that prevented downtime from showing up in the uptime table

--- src/aptostatGui/Service/UptimeService.php
+++ src/aptostatGui/Service/UptimeService.php
@@ -10,15 +10,6 @@ class UptimeService
         $apiService = new ApiService();
         $uptime = $apiService->getUptime();
 
-        foreach ($uptime as $hostname => $errors) {
-            foreach ($errors as $dates => $downtime) {
-
-                $timestamp = substr($dates,0,10);
-                $uptimeStatusAsArray[$hostname][$timestamp] = array_sum($downtime);
-
-            }
-        }
-
-        return $uptimeStatusAsArray;
+        return $uptime;
     }
 }
